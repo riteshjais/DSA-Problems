272c7b86aea92e216b5f5ea2d549bb0d0c373a71
Ritesh Jaiswal <106945647+riteshjais@users.noreply.github.com>
2022-06-07 11:11:20 +0530

    Create Quick-Sort.java

---
+++ Sorting/Quick-Sort.java
@@ -0,0 +1,74 @@
+import java.util.*;
+class Sorting
+{
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    
+    // Driver program
+    public static void main(String args[])
+    {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+			
+			new Solution().quickSort(arr,0,n-1);
+			printArray(arr);
+		    T--;
+		}
+} }// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high)
+    {
+        // code here
+        if(low<high)
+        {
+            int p=partition(arr,low,high);
+            quickSort(arr,low,p-1);
+            quickSort(arr,p+1,high);
+        }
+    }
+    static int partition(int arr[], int low, int high)
+    {
+        // your code here
+        int pivot=arr[high];
+        int i=low-1;
+        for(int j=low;j<=high-1;j++)
+        {
+            if(arr[j]<pivot)
+            {
+                i++;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+            
+        }
+        int temp=arr[i+1];
+        arr[i+1]=arr[high];
+        arr[high]=temp;
+        return (i+1);
+       
+    } 
+    static void swap(int a, int b)
+    {
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+    }
+}
