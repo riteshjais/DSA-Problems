e6f871fc4286694c8d243773416e09fcc1ed0d54
Ritesh Jaiswal <106945647+riteshjais@users.noreply.github.com>
2022-06-10 16:35:42 +0530

    Create Count-Number-of-possible-triangles.java

---
+++ Sorting/Count-Number-of-possible-triangles.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        //taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total count of testcases
+        int t = sc.nextInt();
+        
+        while(t > 0)
+        {
+            //taking elements count
+            int n = sc.nextInt();
+            
+            //creating an array of length n
+            int arr[] = new int[n];
+            
+            //adding elements to the array
+            for(int i = 0; i < n; i++)
+                arr[i] = sc.nextInt();
+            
+            //creating an object of class Solutions
+            Solution ob = new Solution();
+            
+            //calling the method findNumberOfTriangles()
+            //of class Solutions and printing the results
+            System.out.println(ob.findNumberOfTriangles(arr,n));
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to count the number of possible triangles.
+    static int findNumberOfTriangles(int arr[], int n)
+    {
+        // code here
+        Arrays.sort(arr);
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            int k = i + 2;
+            for(int j = i + 1; j < n - 1; j++){
+                while(k < n && arr[k] < arr[i] + arr[j]){
+                    k++;
+                }
+                ans += k - j - 1;
+            }
+        }
+        return ans;
+    }
+}
